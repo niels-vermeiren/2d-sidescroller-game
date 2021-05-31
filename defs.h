616bf67006f50ed65f8f6f2aa9386e68c6433aaa
Unknown <unknown@example.com>
2021-05-31 10:16:50 -0400

    redid level design

--- defs.h
+++ defs.h
@@ -8,7 +8,7 @@
 #define TITLE "2D SideScroller"
 #define SCREEN_WIDTH 64*14
 #define SCREEN_HEIGHT 64*9
-#define LEVEL_HEIGHT 64*18
-#define LEVEL_WIDTH 112 * 64
+#define LEVEL_HEIGHT 64*25
+#define LEVEL_WIDTH 200 * 64
 
 #endif //INC_2DSIDESCROLLER_DEFS_H
